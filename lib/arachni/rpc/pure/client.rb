559401c93e195741ade2633dec498131db0d707e
Tasos Laskos <tasos.laskos@gmail.com>
2014-08-29 19:17:24 +0300

    Arachni-RPC EM => Arachni-RPC Pure

--- lib/arachni/rpc/pure/client.rb
+++ lib/arachni/rpc/pure/client.rb
@@ -1,7 +1,7 @@
 =begin
 
     This file is part of the Arachni-RPC Pure project and may be subject to
-    redistribution and commercial restrictions. Please see the Arachni-RPC EM
+    redistribution and commercial restrictions. Please see the Arachni-RPC Pure
     web site for more information on licensing and terms of use.
 
 =end
